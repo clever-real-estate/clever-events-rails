faa07a183beea56db1376c79f1f4e78e2559d1c1
Nick Clucas <nick@gravy.co>
2025-02-12 11:15:52 -0500

    Initial Commit

---
+++ lib/clever_events_rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clever_events_rails/version"
+
+module CleverEventsRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/clever_events_rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CleverEventsRails do
+  it "has a version number" do
+    expect(CleverEventsRails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
